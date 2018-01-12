2a38817a253069333dd7be4f9120e9e519bb9f31
Denis Hihich <xixi4.den@gmail.com>
2018-01-12 16:29:16 +0300

    create react project
    * configure basic building process via webpack
    * include js transpiling, scss compiling and other.
    * include react lib and compiling jsx files

---
+++ dontmiss-web/webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const paths = {
+  DIST: path.resolve(__dirname, 'dist'),
+  SRC: path.resolve(__dirname, 'src'),
+};
+
+module.exports = {
+  entry: `${paths.SRC}/js/app.js`,
+  output: {
+    path: paths.DIST,
+    filename: 'app.bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      },
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader', 'sass-loader']
+        })
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: `${paths.SRC}/index.html`,
+    }),
+    new ExtractTextPlugin('styles.bundle.css'),
+  ],
+  // resolve: {
+  //   extensions: ['.js', '.jsx']
+  // },
+  devServer: {
+    contentBase: paths.SRC,
+  },
+};
